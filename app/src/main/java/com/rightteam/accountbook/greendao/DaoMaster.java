8b9b2502771408e34f1968ad722e4aa3d2bf7538
JasonWu <jasonwu19960210@gmail.com>
2019-08-18 14:44:13 +0800

    更新gradle

--- app/src/main/java/com/rightteam/accountbook/greendao/DaoMaster.java
+++ app/src/main/java/com/rightteam/accountbook/greendao/DaoMaster.java
@@ -21,14 +21,14 @@ public class DaoMaster extends AbstractDaoMaster {
 
     /** Creates underlying database table using DAOs. */
     public static void createAllTables(Database db, boolean ifNotExists) {
-        WalletBeanDao.createTable(db, ifNotExists);
         BillBeanDao.createTable(db, ifNotExists);
+        WalletBeanDao.createTable(db, ifNotExists);
     }
 
     /** Drops underlying database table using DAOs. */
     public static void dropAllTables(Database db, boolean ifExists) {
-        WalletBeanDao.dropTable(db, ifExists);
         BillBeanDao.dropTable(db, ifExists);
+        WalletBeanDao.dropTable(db, ifExists);
     }
 
     /**
@@ -47,8 +47,8 @@ public class DaoMaster extends AbstractDaoMaster {
 
     public DaoMaster(Database db) {
         super(db, SCHEMA_VERSION);
-        registerDaoClass(WalletBeanDao.class);
         registerDaoClass(BillBeanDao.class);
+        registerDaoClass(WalletBeanDao.class);
     }
 
     public DaoSession newSession() {
